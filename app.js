1d49695f1b873d1b6250494a945b892a31c3ff12
SANG HAN <HanBellevue1@outlook.com>
2017-11-30 14:18:30 -0800

    Yet Another Commit

--- app.js
+++ app.js
@@ -1,75 +1,27 @@
-(function () {
-'use strict';
-
-angular.module('NarrowItDownApp', [])
-  .controller("NarrowItDownController",NarrowItDownController)
-  .service("MenuSearchService",MenuSearchService)
-  .directive("foundItems",foundItems);
-
-NarrowItDownController.$inject = ["MenuSearchService"];
-function NarrowItDownController(MenuSearchService){
-  var vm = this;
-  vm.found = [];
-  vm.search = function(){
-    vm.found = MenuSearchService.getMatchedMenuItems(vm.searchTerm);
-  }
-  vm.remove = function(index){
-    vm.found.splice(index,1);
-  }
-}
-
-MenuSearchService.$inject = ["$http"];
-
-function MenuSearchService($http) {
-  var service = this;
-
-  service.getMatchedMenuItems = function (searchTerm) {
-    if (!service.data) service.getData();
-    if (searchTerm === "") return [];
-    var items = service.data.menu_items;
-    var found = [];
-
-    for (var i=0; i < items.length; i++) {
-      var desc = items[i].description;
-      // console.log(desc);
-      if (desc.indexOf(searchTerm) !== -1){
-        found.push(items[i]);
-      }
-    }
-
-    console.dir(found);
-    return found;
-  };
-
-  service.getData = function() {
-    $http({
-      url: "https://davids-restaurant.herokuapp.com/menu_items.json"
-    }).then(
-      function (result) {
-        console.log(result.data);
-        service.data = result.data;
-      },
-      function (result) {
-        console.log(result.data);
-        service.getData();
+        method: "GET",
+        url: ('https://guilhermego.github.io/menu_items.json')
+        //url: ('https://davids-restaurant.herokuapp.com/menu_items.json')
+      })
+      .then(function (response) {
+        var items = response.data;
+        //var searchItem = 'rice'; // provisório
+        var foundItems = [];
+        // Looping and searching...
+        for (var i = 0; i < items.menu_items.length; i++) {
+          if (items.menu_items[i].name.toLowerCase().indexOf(searchItem) !== -1 ) {
+            foundItems.push(items.menu_items[i]);
+          }
+        }
+        //console.log(service.foundItems);;
+        return foundItems;
+      })
+      .catch(function (response) {
+        console.log("Something went wrong!");
       });
+
+      return response;
+    };
+
   }
 
-  service.getData();
-}
-
-function foundItems(){
-  var ddo = {
-    templateUrl: 'foundItems.html',
-    scope: {
-      items: '<',
-      onRemove: '&'
-    }/*,
-    controller: ShoppingListDirectiveController,
-    controllerAs: 'list',
-    bindToController: true*/
-  };
-
-  return ddo;
-}
 })();
